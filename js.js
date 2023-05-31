9b84cdbca60b4530f7361b1c72d50956eba70860
danyfylth <91761544+danyfylth@users.noreply.github.com>
2023-05-31 12:40:56 -0400

    modificamos el titulo del HTML y agregamos unos comentarios aclarativos de funcionamiento

--- js.js
+++ js.js
@@ -4,6 +4,7 @@
 // Seleccionamos diversos elementos para esta prueba
 const container = document.querySelector(".container");
 
+// * Funcion que crea un elemento item para utilizarlo en el HTML
 const crearItem = (titulo, contenido)=>{
     const item = document.createElement("div");
     const title = document.createElement("h4");
@@ -20,6 +21,7 @@ const crearItem = (titulo, contenido)=>{
     return item;
 };
 
+// * Callback que le sirve de parametro al IntersectionObserver, el cual almacena un arreglo de todos los elementos, que con isIntersecting podemos detectar que el elemento es visible
 const cargarMasItems = entrada => {
     if (entrada[0].isIntersecting) {
         cargarItems(2);
@@ -27,22 +29,30 @@ const cargarMasItems = entrada => {
 }
 
 const observador = new IntersectionObserver(cargarMasItems);
-
+// * Contador general del arreglo de publicaciones
 var contador = 0;
 const cargarItems = async num =>{
+    // * Devolvemos una promesa
     const request = await fetch("informacion.txt");
+    // * Convertimos a JSON
     const items = await request.json();
+    // * Obtenemos el arreglo de las publicaciones que se encuentran en la clave 'items'
     const arr = items.items;
+    // * Creamos un fragmento de enmpaquetado
     const fragment = document.createDocumentFragment();
+    // * ESte for es la cantidad de items que deseamos renderizar por cada interseccion
     for (let i = 0; i < num; i++) {
         if (arr[contador] != undefined) {
             const newItem = crearItem(arr[contador].title, arr[contador].content);
             fragment.appendChild(newItem);
             contador++;
-            if (i == num-1) {
+            // * Si es el ultimo item, generamos otra cantidad de items gracias al observador volvemos a ejecutar esta función
+            if (i == num - 1) {
+                // * Una vez definido el observador este esta vinculado a nivel de ejecución eso significa que al observar los itemas seleccionados ejecutar las acciones del Callback.
                 observador.observe(newItem);
             }
         }else{
+            // * Si ya no hay mas items que cargar, verficamos que no exista la caja de "No hay más publicaciones." para poder crearlo una sola vez. Esto para evitar que se vuelva a ejecutar el observador con los otros items de arriba, esto signifca que si volvemos a esos items se volvera a ejecutar esta funcion.
             if (container.lastElementChild.id != "nomore") {
                 let noMore = document.createElement("h4");
                 noMore.textContent = "No hay más publicaciones.";
